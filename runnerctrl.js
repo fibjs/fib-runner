1fc951b6395d34c385c4c7625168fdc8a115a0c9
xicilion <lion@9465.net>
2021-09-01 22:08:10 +0800

    ruler

--- runnerctrl.js
+++ runnerctrl.js
@@ -41,15 +41,71 @@ function list() {
     }));
 }
 
+function list_rule(tm, length, interval) {
+    var line = '      ───┼'
+    var rule;
+    var _date = interval >= 60;
+    var mark_interval;
+    var base_tm = tm - length + 1;
+
+    function put_mark(pos) {
+        var d = new Date((base_tm + pos) * interval * 60000);
+        var _mark = _date ? (' ' + d.getDate()).slice(-2) + '/' + ((d.getMonth() + 1) + ' ').slice(0, 2)
+            : (' ' + d.getHours()).slice(-2) + ':' + ('0' + d.getMinutes()).slice(-2);
+
+        if (pos > 0)
+            line = line.substr(0, pos + 9) + '┬' + line.substr(pos + 10);
+        rule = rule.substr(0, pos + 7) + _mark + rule.substr(pos + 12);
+    }
+
+    switch (Number(interval)) {
+        case 1:
+            mark_interval = 15;
+            break;
+        case 5:
+            mark_interval = 60;
+            break;
+        case 15:
+            mark_interval = 180;
+            break;
+        case 60:
+            mark_interval = 1440;
+            break;
+        case 240:
+            mark_interval = 5760;
+            break;
+        case 720:
+            mark_interval = 17280;
+            break;
+    }
+
+    line = line + '─'.repeat(length + 1);
+    rule = ' '.repeat(length + 11);
+
+    put_mark(0);
+    if (length > 6) {
+        put_mark(length - 1);
+
+        var pos = (Math.floor((base_tm + 7) * interval / mark_interval) + 1) * mark_interval / interval - base_tm;
+
+        while (pos < length - 7) {
+            put_mark(pos);
+            pos += mark_interval / interval;
+        }
+    }
+
+    return line + '\n' + rule;
+}
+
 function list_usage(name, interval, type) {
-    var padding = '       ';
+    var padding = '        ';
 
     function format_cpu(x) {
-        return (padding + (x * 100).toFixed(2)).slice(-padding.length) + '%';
+        return (padding + (x * 100).toFixed(2) + '%').slice(-padding.length);
     }
 
     function format_mem(x) {
-        return (padding + (x / (1024 * 1024)).toFixed(2)).slice(-padding.length) + ' MB';
+        return (padding + (x / (1024 * 1024)).toFixed(1) + 'M').slice(-padding.length);
     }
 
     interval = interval || 1;
@@ -59,10 +115,15 @@ function list_usage(name, interval, type) {
     }
 
     var r = http.get(`http://127.0.0.1:13828/${type}/${name}/${interval}`).json();
+
+    var columns = process.stdout.columns;
+    r.usage = r.usage.slice(-(columns - 12));
+
     console.log(asciichart.plot(r.usage, {
         height: 10,
         format: type == 'cpu' ? format_cpu : format_mem
     }));
+    console.log(list_rule(r.tm, r.usage.length, interval));
 }
 
 function log(name, length) {
